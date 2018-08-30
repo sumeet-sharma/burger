cf9c9aacf8e14fef9f95415b836d35246d35eb97
Sumeet <sumeetdutt@gmail.com>
2018-08-30 04:45:00 -0400

    config and db done

---
+++ config/orm.js
@@ -0,0 +1,60 @@
+var connection = require('./connection.js');
+
+// Object Relational Mapper (ORM)
+
+// The ?? signs are for swapping out table or column names
+// The ? signs are for swapping out other values
+// These help avoid SQL injection
+// https://en.wikipedia.org/wiki/SQL_injection
+// Object for all our SQL statement functions.
+var orm = {
+    all: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    create: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table + "()";
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    // An example of objColVals would be {name: panther, sleepy: true}
+    update: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+};
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheese Burger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon and Cheddar Angus Burger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', 0);
