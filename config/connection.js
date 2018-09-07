52399dfec2b1785a8fbd73798dedcde7a5788bb3
manjeet chopra <sumeetdutt@gmail.com>
2018-09-07 18:56:15 -0400

    heroku debugging

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,13 @@
 var mysql = require('mysql');
 var connection;
 
-// Setting up connection for use on Heroku
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
 // Localhost connection
-else {
-  connection = mysql.createConnection({
+connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : 'root',
     database : 'burgers_db',
     port: 3306
   });
-};
 
 module.exports = connection; 
